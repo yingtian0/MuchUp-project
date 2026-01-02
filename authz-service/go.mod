15d118892a0215175689c8a9efd88a313162471e
yingtian0 <haidouyingtian0@gmail.com>
2026-01-03 02:20:29 +0900

    add:redisClientの排他制御の実装

--- authz-service/go.mod
+++ authz-service/go.mod
@@ -4,6 +4,7 @@ go 1.25.1
 
 require (
 	github.com/golang-jwt/jwt/v5 v5.3.0
+	github.com/google/uuid v1.6.0
 	github.com/redis/go-redis/v9 v9.17.2
 )
 
--- authz-service/redis.go
+++ authz-service/redis.go
@@ -2,7 +2,11 @@ package authzservice
 
 import (
 	"context"
+	"fmt"
 	"os"
+	"time"
+
+	"github.com/google/uuid"
 
 	"github.com/redis/go-redis/v9"
 )
@@ -12,9 +16,35 @@ var (
 	rdb *redis.Client
 )
 
+var unlockScript = redis.NewScript(`
+if redis.call("GET", KEYS[1]) == ARGV[1] then
+    return redis.call("DEL", KEYS[1])
+else
+    return 0
+end
+`)
+
 func InitRedis () {
 	rdb = redis.NewClient(&redis.Options{
 		Addr: os.Getenv("REDIS_PORT"),
 		Password: "",
 		DB: 0,})
 }
+
+
+func LockProccss(rdb redis.Client,key string,ttl time.Duration) (string,bool,error) {
+
+	value := uuid.NewString()
+	ok,err := rdb.SetNX(ctx,value,key,ttl).Result()
+	if err != nil || !ok {
+		return "",false,fmt.Errorf("faild to lock Redis process:%w",err)
+	}
+
+	return value,true,nil
+}
+
+func UnlockProcess(rdb redis.Client,key,value string) error {
+	_, err := unlockScript.Run(ctx, rdb, []string{key}, value).Result()
+	return err
+}
+
