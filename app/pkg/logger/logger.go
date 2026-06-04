b2e43151cf628710c594ba87a44cc3d5430c8bcd
yingtian <haidouyingtian@gmail.com>
2026-06-04 21:57:29 +0900

    fix: logger変数名の修正

--- app/pkg/logger/logger.go
+++ app/pkg/logger/logger.go
@@ -24,13 +24,13 @@ type Logger interface {
 	Errorf(format string, args ...interface{})
 	Fatalf(format string, args ...interface{})
 }
-type Struct struct {
+type AppLogger struct {
 	logger *zerolog.Logger
 }
 
-var _ Logger = (*Struct)(nil)
+var _ Logger = (*AppLogger)(nil)
 
-func New(level string) *Struct {
+func New(level string) *AppLogger {
 	var l zerolog.Level
 
 	switch strings.ToLower(level) {
@@ -51,68 +51,68 @@ func New(level string) *Struct {
 	skipFrameCount := 3
 	logger := zerolog.New(os.Stdout).With().Timestamp().CallerWithSkipFrameCount(zerolog.CallerSkipFrameCount + skipFrameCount).Logger()
 
-	return &Struct{
+	return &AppLogger{
 		logger: &logger,
 	}
 }
 func NewLogger() Logger {
 	return New("info")
 }
-func (l *Struct) Debug(msg string, args ...interface{}) {
+func (l *AppLogger) Debug(msg string, args ...interface{}) {
 	l.logger.Debug().Msgf(msg, args...)
 }
-func (l *Struct) Info(msg string, args ...interface{}) {
+func (l *AppLogger) Info(msg string, args ...interface{}) {
 	l.logger.Info().Msgf(msg, args...)
 }
-func (l *Struct) Warn(msg string, args ...interface{}) {
+func (l *AppLogger) Warn(msg string, args ...interface{}) {
 	l.logger.Warn().Msgf(msg, args...)
 }
-func (l *Struct) Error(msg string, args ...interface{}) {
+func (l *AppLogger) Error(msg string, args ...interface{}) {
 	l.logger.Error().Msgf(msg, args...)
 }
-func (l *Struct) Fatal(msg string, args ...interface{}) {
+func (l *AppLogger) Fatal(msg string, args ...interface{}) {
 	l.logger.Fatal().Msgf(msg, args...)
 	os.Exit(1)
 }
-func (l *Struct) WithContext(_ context.Context) Logger {
-	return &Struct{
+func (l *AppLogger) WithContext(_ context.Context) Logger {
+	return &AppLogger{
 		logger: l.logger,
 	}
 }
-func (l *Struct) WithError(err error) Logger {
+func (l *AppLogger) WithError(err error) Logger {
 	newLogger := l.logger.With().Err(err).Logger()
 
-	return &Struct{
+	return &AppLogger{
 		logger: &newLogger,
 	}
 }
-func (l *Struct) WithField(key string, value interface{}) Logger {
+func (l *AppLogger) WithField(key string, value interface{}) Logger {
 	newLogger := l.logger.With().Interface(key, value).Logger()
 
-	return &Struct{
+	return &AppLogger{
 		logger: &newLogger,
 	}
 }
-func (l *Struct) WithFields(fields map[string]interface{}) Logger {
+func (l *AppLogger) WithFields(fields map[string]interface{}) Logger {
 	newLogger := l.logger.With().Fields(fields).Logger()
 
-	return &Struct{
+	return &AppLogger{
 		logger: &newLogger,
 	}
 }
-func (l *Struct) Debugf(format string, args ...interface{}) {
+func (l *AppLogger) Debugf(format string, args ...interface{}) {
 	l.logger.Debug().Msgf(format, args...)
 }
-func (l *Struct) Infof(format string, args ...interface{}) {
+func (l *AppLogger) Infof(format string, args ...interface{}) {
 	l.logger.Info().Msgf(format, args...)
 }
-func (l *Struct) Warnf(format string, args ...interface{}) {
+func (l *AppLogger) Warnf(format string, args ...interface{}) {
 	l.logger.Warn().Msgf(format, args...)
 }
-func (l *Struct) Errorf(format string, args ...interface{}) {
+func (l *AppLogger) Errorf(format string, args ...interface{}) {
 	l.logger.Error().Msgf(format, args...)
 }
-func (l *Struct) Fatalf(format string, args ...interface{}) {
+func (l *AppLogger) Fatalf(format string, args ...interface{}) {
 	l.logger.Fatal().Msgf(format, args...)
 	os.Exit(1)
 }
