5b53e1828fdae2631716d83c19a5ce6caafded21
Retasusan <kenta9736@gmail.com>
2026-06-14 18:40:13 +0900

    add type:uuid to senderid

--- app/internal/infrastructure/database/schema/message_schema.go
+++ app/internal/infrastructure/database/schema/message_schema.go
@@ -7,9 +7,9 @@ import (
 )
 
 type MessageSchema struct {
-	ID        string `gorm:"type:uuid;primaryKey"`
-	Text      string `gorm:"type:text;not null"`
-	SenderID  *string
+	ID        string     `gorm:"type:uuid;primaryKey"`
+	Text      string     `gorm:"type:text;not null"`
+	SenderID  *string    `gorm:"type:uuid"`
 	RoomID    string     `gorm:"type:uuid"`
 	Room      RoomSchema `gorm:"foreignKey:RoomID"`
 	CreatedAt time.Time
